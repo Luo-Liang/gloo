db1daa7ed374a9649382c374d136b53abfe12651
Ubuntu <ubuntu@ip-172-31-1-69.us-west-2.compute.internal>
2018-08-24 06:20:35 +0000

    allreduce_phub algorithm in gloo

--- gloo/allreduce_phub.h
+++ gloo/allreduce_phub.h
@@ -11,55 +11,159 @@
 
 namespace gloo
 {
-std::string pHubGetEnvironmemtVariable(std::string &name)
+std::string pHubGetMandatoryEnvironmemtVariable(std::string &name)
+{
+    var val = pHubGetOptionalEnvironmentVariable(name);
+    CHECK(val != "") << name << " is not set in environment variable";
+    return val;
+}
+
+std::string pHubGetOptionalEnvironmentVariable(std::string &name)
 {
     var val = std::getenv(name.c_str());
-    CHECK(val != NULL) << name << " is not set in environment variable";
-    return std::string(val);
+    var ret = val == NULL ? std::string("") : std::string(val);
+    return val;
 }
 
 template <typename T>
 class AllReducePHub : public Algorithm
 {
   private:
-    std::shared_ptr<PHub> backend;
+    std::shared_ptr<PHub> pHub;
+    std::vector<T *> ptrs_;
+    int dataElementCount;
 
   public:
     AllReducePHub(
         const std::shared_ptr<::gloo::Context> &context,
         const std::vector<T *> ptrs,
         const int count,
-        const ReductionFunction<T> *fn = ReductionFunction<T>::sum) : Algorithm(context)
+        const ReductionFunction<T> *fn = ReductionFunction<T>::sum) : Algorithm(context), dataElementCount(count), ptrs_(ptrs)
     {
         //context is not used.
         //this is because PHub uses a separate way of performing rendezvous.
         //but size and rank is still used.
+        if (context->size == 1)
+            return;
         var redisHost = pHubGetEnvironmemtVariable("PHubRedisHost");
         std::string redisIp;
         ushort redisPort;
         ParseHostPort(redisHost, redisIp, redisPort);
-        //PHub requests a node IP map. Gloo does this automatically, 
+        //PHub requests a node IP map. Gloo does this automatically,
         //so we need to use PhubRendezvous to retrieve one.
         std::vector<NodeIp> participants;
-        for(int i = 0; i < context->size; i++)
+        for (int i = 0; i < context->size; i++)
         {
             participants.push_back((NodeId)i);
         }
         PHubRendezvous rendezvous(redisIp, redisPort, context->rank);
         std::string myIp = trim(PHubExecute("hostname -i"));
-        var nodeMap =rendezvous->PullNodeMap(participants, myIp);
+        var nodeMap = rendezvous->PullNodeMap(participants, myIp);
         //remember to chunk my keys
-        
+
         var chunkSize = atoi(pHubGetEnvironmemtVariable("PHubChunkElementSize").c_str());
-        T* ptr = ptrs.at(0);
-        
-        int chunkCount = (int)ceil(count * 1.0 / chunkSize);
+
+        int keyCount = (int)ceil(count * 1.0 / chunkSize);
         int chunksSizeInBytes = chunkSize * sizeof(T);
+        int remainderInBytes = (count % chunkSize) * sizeof(T);
         CHECK(sizeof(T) == 4) << "Currently PHub only supports 4 Byte floats";
         std::vector<float> keySizes;
-        std::vector<void*> appAddrs;
+        std::vector<void *> appAddrs;
+        T *ptr = ptrs.at(0);
+
+        //now initialize keySizes.
+        T *currPtr = ptr;
+        for (PLinkKey key = 0; key < keyCount; key++)
+        {
+            int sizeInBytes = (key == keyCount - 1) && remainderInBytes ? chunksSizeInBytes;
+            int elementCount = sizeInBytes / sizeof(T);
+            keySizes.push_back(sizeInBytes);
+            appAddrs.push_back(currPtr);
+            currPtr += elementCount;
+        }
+        CHECK(currPtr == ptr + count) << "There is a problem assigning appAddr for PHub";
+
         PHubLaunchPreference preference;
-        backend = std::make_shared<PHub>(redisHost, nodeMap, keySizes, appAddrs, participants.size(), sizeof(T), context->rank, preference);
+#pragma region
+        preference.BlockedInterfaces = pHubGetOptionalEnvironmentVariable("PHubBlockedInterfaces");
+        if (pHubGetOptionalEnvironmentVariable("PHubCoreOffset") != "")
+        {
+            preference.CoreMapLaunchOffset = atoi(pHubGetOptionalEnvironmentVariable("PHubCoreOffset").c_str());
+        }
+        if (pHubGetOptionalEnvironmentVariable("PHubCQDepth") != "")
+        {
+            preference.CQDepth = atoi(pHubGetOptionalEnvironmentVariable("PHubCQDepth").c_str());
+        }
+        if (pHubGetOptionalEnvironmentVariable("PHubCQSendPollFrequency") != "")
+        {
+            preference.CQSendPollFrequency = atoi(pHubGetOptionalEnvironmentVariable("PHubCQSendPollFrequency").c_str());
+        }
+        preference.DisableHyperThreadingWarning = pHubGetOptionalEnvironmentVariable("PHubDisableHyperThreadingWarning") != "";
+        preference.DisableNumaAwareness = pHubGetOptionalEnvironmentVariable("PHubDisableNumaAwareness") != "";
+        if (pHubGetOptionalEnvironmentVariable("PHubMaximumCore") != "")
+        {
+            preference.MaximumCores = atoi(pHubGetOptionalEnvironmentVariable("PHubMaximumCore").c_str());
+        }
+        if (pHubGetOptionalEnvironmentVariable("PHubMinRNRTimer") != "")
+        {
+            preference.MinRNRTimer = atoi(pHubGetOptionalEnvironmentVariable("PHubMinRNRTimer").c_str());
+        }
+        if (pHubGetOptionalEnvironmentVariable("PHubCoreAssignmentScheme") != "")
+        {
+            preference.PHubCoreAssignmentScheme = pHubGetOptionalEnvironmentVariable("PHubCoreAssignmentScheme");
+        }
+        if (pHubGetOptionalEnvironmentVariable("PHubIFAssignmentScheme") != "")
+        {
+            preference.PHubIFAssignmentScheme = pHubGetOptionalEnvironmentVariable("PHubIFAssignmentScheme");
+        }
+        if (pHubGetOptionalEnvironmentVariable("PHubPreferredGidIndex") != "")
+        {
+            preference.PreferredGidIndex = atoi(pHubGetOptionalEnvironmentVariable("PHubPreferredGidIndex").c_str());
+        }
+        if (pHubGetOptionalEnvironmentVariable("PHubQPPerDeviceConnection") != "")
+        {
+            preference.QPPerDeviceConnection = atoi(pHubGetOptionalEnvironmentVariable("PHubQPPerDeviceConnection").c_str());
+        }
+        if (pHubGetOptionalEnvironmentVariable("PHubRetryCount") != "")
+        {
+            preference.RetryCount = atoi(pHubGetOptionalEnvironmentVariable("PHubRetryCount").c_str());
+        }
+        if (pHubGetOptionalEnvironmentVariable("PHubRNRRetry") != "")
+        {
+            preference.RNRRetry = atoi(pHubGetOptionalEnvironmentVariable("PHubRNRRetry").c_str());
+        }
+        if (pHubGetOptionalEnvironmentVariable("PHubTransmitTimeout") != "")
+        {
+            preference.TransmitTimeout = atoi(pHubGetOptionalEnvironmentVariable("PHubTransmitTimeout").c_str());
+        }
+        preference.UseiWarp = pHubGetOptionalEnvironmentVariable("PHubUseiWarp") != "";
+
+#pragma endregion
+        pHub = std::make_shared<PHub>(redisHost, nodeMap, keySizes, appAddrs, participants.size(), sizeof(T), context->rank, preference);
+    }
+
+    void run()
+    {
+        size_t bufferOffset = 0;
+        size_t numItems =
+            stepsWithinBlock_ > 0 ? chunkSize_ << (steps_ - 1) : count_;
+
+        for (int i = 1; i < ptrs_.size(); i++)
+        {
+            fn_->call(ptrs_[0], ptrs_[i], count_);
+        }
+        if (this->contextSize_ == 1)
+        {
+            // Broadcast ptrs_[0]
+            for (int i = 1; i < ptrs_.size(); i++)
+            {
+                memcpy(ptrs_[i], ptrs_[0], bytes_);
+            }
+            return;
+        }
+
+        //simply call PHub Reduce.
+        pHub->Reduce();
     }
 };
 } // namespace gloo
