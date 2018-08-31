a2b52ac29131df244d559edca80eba270e3d63c8
Ubuntu <ubuntu@ip-172-31-1-69.us-west-2.compute.internal>
2018-08-31 00:19:35 +0000

    gloo auto update script

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+git remote update
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+git reset --hard origin/master
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+    git pull
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
+fi
