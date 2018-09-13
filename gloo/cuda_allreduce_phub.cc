a89b2b239f4fc84927344894561f80d242ca7b2a
Ubuntu <ubuntu@ip-172-31-47-178.us-west-2.compute.internal>
2018-09-13 00:35:34 +0000

    show more context creation information

--- gloo/cuda_allreduce_phub.cc
+++ gloo/cuda_allreduce_phub.cc
@@ -50,7 +50,7 @@ CudaAllreducePHub<T, W>::CudaAllreducePHub(
     return;
   }
   T *ptr = *inbox_;
-  fprintf(stderr, "initializing PHub with context[%d](%p). .size=%d .rank=%d\n", context->getCID(), context.get(), context->size, context->rank);
+  fprintf(stderr, "initializing PHub with context[%d](%p). .size=%d .rank=%d .ptr=%p, .count=%d\n", context->getCID(), context.get(), context->size, context->rank, ptr, count);
   pHub = createPHubInstance(ptr, count, context->size, context->rank, context->getCID());
 }
 // namespace gloo
