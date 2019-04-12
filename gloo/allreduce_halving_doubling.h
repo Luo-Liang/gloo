040807cd4e9fed1b6a66783a0064121c4ce7d276
Ubuntu <ubuntu@ip-172-31-18-185.ec2.internal>
2019-04-12 19:53:12 +0000

    verify encoding correctness

--- gloo/allreduce_halving_doubling.h
+++ gloo/allreduce_halving_doubling.h
@@ -62,6 +62,7 @@ namespace gloo {
 
 			stepsWithinBlock_ = log2(myBinaryBlockSize_);
 			rankInBinaryBlock_ = this->context_->rank % myBinaryBlockSize_;
+			printf("myBinaryBlockSize_ = %d, nextSmallerBlockSize_ = %d, nextLargerBlockSize_ = %d\n", myBinaryBlockSize_, nextSmallerBlockSize_,nextLargerBlockSize_);
 		}
 
 	public:
