7e0de3b7f780240a9aa91afd0dfb5a938e23671d
ubuntu <ubuntu>
2019-08-21 07:51:57 +0000

    remove sim

--- gloo/cuda_allreduce_phub.cc
+++ gloo/cuda_allreduce_phub.cc
@@ -19,26 +19,6 @@ CudaAllreducePHub<T, W>::CudaAllreducePHub(
       synchronizeDeviceOutputs_(streams.size() == 0),
       fn_(CudaReductionFunction<T>::sum)
 {
-  const char* qRatioPtr = std::getenv("PHUB_QUANTIZATION_SIM_RATIO");
-  if(qRatioPtr != nullptr)
-  {
-      int qRatio = atoi(qRatioPtr);
-      if(qRatio!=1)
-      {
-          fprintf(stderr, "warning: a quantization ratio of %d is specified.\n", qRatio);
-          int elements = count_;
-          int remElements = elements / qRatio;
-          int elementWidth = sizeof(T);
-          if(remElements == 0)
-          {
-	        remElements = 1;
-          }
-          bytes_  = remElements * elementWidth;
-          count_ = remElements;
-          //chunkSize_ = (count_ + chunks_ - 1) / chunks_;
-          //chunkBytes_ = chunkSize_ * sizeof(T);
-      }
-  }
   auto newStream = true;
   if (streams.size() > 0)
   {
